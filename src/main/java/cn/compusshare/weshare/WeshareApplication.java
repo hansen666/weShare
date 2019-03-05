d79fc40e1348046afa625bb57b379f2e1bcf841e
on-loding <306863246@qq.com>
2019-03-05 21:22:44 +0800

    更新到dev的最新内容

--- src/main/java/cn/compusshare/weshare/WeshareApplication.java
+++ src/main/java/cn/compusshare/weshare/WeshareApplication.java
@@ -3,9 +3,11 @@ package cn.compusshare.weshare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableAsync
 public class WeshareApplication {
 
 	public static void main(String[] args) {
