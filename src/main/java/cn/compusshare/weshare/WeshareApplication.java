79829ca893e9f45b12354aa94f91ebf5ffdc175d
TCZdudu <1456164189@qq.com>
2019-03-07 15:06:47 +0800

    selectSchool 方法

--- src/main/java/cn/compusshare/weshare/WeshareApplication.java
+++ src/main/java/cn/compusshare/weshare/WeshareApplication.java
@@ -1,12 +1,10 @@
 package cn.compusshare.weshare;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableAsync
 public class WeshareApplication {
 
