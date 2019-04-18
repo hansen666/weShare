398f51292a26260cb9b4efa7b9b620d5f0612af1
on-loding <306863246@qq.com>
2019-04-18 10:03:20 +0800

    跨域配置方式变更

---
+++ src/main/java/cn/compusshare/weshare/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package cn.compusshare.weshare.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+/**
+ * @Author: LZing
+ * @Date: 2019/4/18
+ * 跨域请求配置
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+
+    }
+}
